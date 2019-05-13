2645583dbcbf2efbcf2d3ef6705d5d3f4a26907d
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2019-05-14 00:23:16 +0700

    Use MIT license instead of GPL

--- pullover.el
+++ pullover.el
@@ -3,20 +3,11 @@
 ;; Copyright (C) 2019  Tuấn-Anh Nguyễn
 ;;
 ;; Author: Tuấn-Anh Nguyễn <ubolonton@gmail.com>
-;; Keywords:
+;; Keywords: convenience, mac
 ;; Homepage: https://github.com/ubolonton/pullover
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "25.1"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; License: MIT
 
 ;;; Commentary:
 
